6b22d79e3deaa21b3f7d1f34920fe1db85587e6a
vitoriajanaina02 <vitoriajanaina3@gmail.com>
2026-06-25 07:44:35 -0300

    atualizando pasta aula 02

--- Aula 02/tipos_de_operadores.py
+++ Aula 02/tipos_de_operadores.py
@@ -143,7 +143,7 @@ saldo %= 480
 
 print(saldo) # 20
 
-# Atribuição com wxponenciação
+# Atribuição com exponenciação
 saldo = 80
 saldo **= 2
 
@@ -151,3 +151,84 @@ print(saldo) # 6400
 
 
 # OPERADORES LÓGICOS
+
+# O que são:
+# São operadores utilizados em conjunto com os operadores de comparação, para montar uma expressão lógica. Quando um
+# operador de comparação é utilizado, o resultado retornado é um booleano, dessa forma podemos combinar operadores de
+# comparação com os operadores lógicos, exemplo: op_comparacao + op_logico + op_comparacao...N...
+
+# EXEMPLO
+saldo = 1000
+saque = 200
+limite = 100
+
+saldo >= saque # true
+saque <= limite # false
+
+# Operador E
+saldo = 1000
+saque = 200
+limite = 100
+
+saldo >= saque and saque <= limite # false
+
+# Operador OU
+saldo = 1000
+saque = 200
+limite = 100
+
+saldo >= saque or saque <= limite # true
+
+# Operador Negação
+contatos_emergencia = []
+
+not 1000 > 1500 # true
+
+not contatos_emergencia # true
+
+not "saque 1500;" # false
+
+not "" # true
+
+# Parênteses
+saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
+
+saldo >= saque and saque <= limite or conta_especial and saldo >= saque # true
+
+(saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque) # true
+
+
+# OPERADORES DE IDENTIDADE
+
+# O que são:
+# São operadores utilizados para comparar se os dois objetos testados ocupam a mesma posição na memória.
+
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+
+curso is nome_curso # true
+
+curso is not nome_curso # false
+
+saldo is limite # true
+
+
+# OPERADORES DE ASSOCIAÇÃO
+
+# O que são:
+# São operadores utilizados para verificar se um objeto está presente em uma sequência.
+
+# EXEMPLO
+curso = "Aprendizado de Máquina"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+"Aprendizado" in curso # true
+
+"maça" not in frutas # true
+
+200 in saques # false
